5a76e1015993630158ff6ff100a8a3b70b9feb52
Pol Dellaiera <pol.dellaiera@protonmail.com>
2021-07-02 22:44:05 +0200

    chore: Update PHPCSFixer config.

--- .php-cs-fixer.dist.php
+++ .php-cs-fixer.dist.php
@@ -1,9 +1,12 @@
 <?php
 
+/**
+ * For the full copyright and license information, please view
+ * the LICENSE file that was distributed with this source code.
+ */
+
+declare(strict_types=1);
+
 $config = require __DIR__ . '/vendor/drupol/php-conventions/config/php73/php_cs_fixer.config.php';
-
-$rules = $config->getRules();
-
-$rules['strict_comparison'] = false;
 
 return $config->setRules($rules);
