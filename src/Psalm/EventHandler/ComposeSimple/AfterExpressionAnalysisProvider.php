749aa79be4c6bbd02e15fa9d8e585fa49dd29100
Pol Dellaiera <pol.dellaiera@protonmail.com>
2021-07-30 22:09:51 +0200

    Work in progress

--- src/Psalm/EventHandler/ComposeSimple/AfterExpressionAnalysisProvider.php
+++ src/Psalm/EventHandler/ComposeSimple/AfterExpressionAnalysisProvider.php
@@ -9,13 +9,59 @@ declare(strict_types=1);
 
 namespace loophp\fpt\Psalm\EventHandler\ComposeSimple;
 
-use Psalm\Plugin\EventHandler\AfterExpressionAnalysisInterface;
-use Psalm\Plugin\EventHandler\Event\AfterExpressionAnalysisEvent;
+use PhpParser\Node;
+use PhpParser\Node\FunctionLike;
+use PhpParser\NodeTraverser;
+use PhpParser\NodeVisitorAbstract;
+use Psalm\Internal\Analyzer\MethodAnalyzer;
+use Psalm\Plugin\EventHandler\AfterFunctionLikeAnalysisInterface;
+use Psalm\Plugin\EventHandler\Event\AfterFunctionLikeAnalysisEvent;
 
-final class AfterExpressionAnalysisProvider implements AfterExpressionAnalysisInterface
+final class AfterExpressionAnalysisProvider implements AfterFunctionLikeAnalysisInterface
 {
-    public static function afterExpressionAnalysis(AfterExpressionAnalysisEvent $event): ?bool
-    {
+
+    public static function afterStatementAnalysis(AfterFunctionLikeAnalysisEvent $event): ?bool {
+        $stmtSource = $event->getStatementsSource();
+
+        if (false === ($stmtSource instanceof MethodAnalyzer)) {
+            return false;
+        }
+
+        if ('of' !== $stmtSource->getMethodName()) {
+            return false;
+        }
+
+        $ast = $event->getStmt();
+
+        if (false === ($ast instanceof FunctionLike)) {
+            return false;
+        }
+
+        $closure = self::getNextClosure($ast);
+
+        // This is the $fs param of ComposeSimple::of()(callable ...$fs);
+        // ----------------------------------------------------------^
+        $param = current($closure->getParams());
+
+        // Questions
+        // 1. What to do from there?
+
         return true;
+    }
+
+    private static function getNextClosure($ast): Node\Expr\Closure
+    {
+        $traverser = new NodeTraverser();
+
+        $traverser->addVisitor(new class extends NodeVisitorAbstract {
+            public function enterNode(Node $node): ?Node\Stmt\Return_ {
+                return $node instanceof Node\Stmt\Return_ ? $node : null;
+            }
+        });
+
+        /** @var array<int, \PhpParser\Node\Stmt\Return_> $returnStatement */
+        $returnStatement = $traverser->traverse($ast->getStmts());
+
+        return current($returnStatement)->expr;
     }
 }
